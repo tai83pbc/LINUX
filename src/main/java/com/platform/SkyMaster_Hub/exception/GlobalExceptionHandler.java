bd9a66975d63b46d72580ae92156927e665caa43
Tati83pbc <an607772@gmail.com>
2026-01-23 17:00:16 +0700

    code with 404 update 3

--- src/main/java/com/platform/SkyMaster_Hub/exception/GlobalExceptionHandler.java
+++ src/main/java/com/platform/SkyMaster_Hub/exception/GlobalExceptionHandler.java
@@ -1,13 +1,13 @@
 package com.platform.SkyMaster_Hub.exception;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,13 +22,13 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
     
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, Object>> handleGeneralException(Exception ex) {
-        Map<String, Object> errorResponse = new HashMap<>();
-        errorResponse.put("timestamp", LocalDateTime.now());
-        errorResponse.put("message", "An error occurred: " + ex.getMessage());
-        errorResponse.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+    // @ExceptionHandler(Exception.class)
+    // public ResponseEntity<Map<String, Object>> handleGeneralException(Exception ex) {
+    //     Map<String, Object> errorResponse = new HashMap<>();
+    //     errorResponse.put("timestamp", LocalDateTime.now());
+    //     errorResponse.put("message", "An error occurred: " + ex.getMessage());
+    //     errorResponse.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
         
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    //     return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    // }
 }
