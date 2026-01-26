98672a30953156fc53e701fe77dbecc783fe81a4
Tati83pbc <an607772@gmail.com>
2026-01-26 15:53:55 +0700

    add erro controller

---
+++ src/main/java/com/platform/SkyMaster_Hub/controller/CustomErrorController.java
@@ -0,0 +1,27 @@
+package com.platform.SkyMaster_Hub.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+            
+            if (statusCode == 404) {
+                return "forward:/404.html";
+            }
+        }
+        
+        return "error";
+    }
+}
